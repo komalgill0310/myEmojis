c71a025f38cc79ffa2eaeeb86b589a8f73e4b10e
Komalpreet Kaur <80730790+komalgill0310@users.noreply.github.com>
2023-03-31 23:54:34 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const myEmojis = ["👨‍💻", "📖", "🏸", "🎳", "♟️","🥣"]
+const myEmojis = ["👨‍💻", "📖", "🏸", "🎳", "♟️","🥣","🍔"]
 
 function renderEmojis() {
     const emojiContainer = document.getElementById("emoji-container")
